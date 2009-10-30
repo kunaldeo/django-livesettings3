02e91903332b1cee735680e74ff394943af9c1ff
Bruce Kroeze <bkroeze@gmail.com>
2009-10-29 20:29:05 -0700

    adding tests

---
+++ tests/settings.py
@@ -0,0 +1,95 @@
+# Django settings for satchmo project.
+# If you have an existing project, then ensure that you modify local_settings-customize.py
+# and import it from your main settings file. (from local_settings import *)
+import os
+
+DIRNAME = os.path.dirname(__file__)
+
+DJANGO_PROJECT = 'tests'
+DJANGO_SETTINGS_MODULE = 'tests.settings'
+
+ADMINS = (
+     ('', ''),
+)
+
+MANAGERS = ADMINS
+
+DATABASE_ENGINE = 'sqlite3'           # 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
+DATABASE_NAME = 'test.db'             # Or path to database file if using sqlite3.
+
+# Local time zone for this installation. All choices can be found here:
+# http://www.postgresql.org/docs/current/static/datetime-keywords.html#DATETIME-TIMEZONE-SET-TABLE
+TIME_ZONE = 'US/Pacific'
+
+# Language code for this installation. All choices can be found here:
+# http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
+# http://blogs.law.harvard.edu/tech/stories/storyReader$15
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+#Image files will be stored off of this path
+MEDIA_ROOT = os.path.join(DIRNAME, 'static/')
+#MEDIA_ROOT = "/static"
+# URL that handles the media served from MEDIA_ROOT.
+# Example: "http://media.lawrence.com"
+#MEDIA_URL = 'site_media'
+MEDIA_URL="/static/"
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/media/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = ''
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+#     'django.template.loaders.eggs.load_template_source',
+)
+
+MIDDLEWARE_CLASSES = (
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.middleware.doc.XViewMiddleware",
+    #'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+#this is used to add additional config variables to each request
+# NOTE: overridden in local_settings.py
+# NOTE: If you enable the recent_products context_processor, you MUST have the
+# 'satchmo_ext.recentlist' app installed.
+TEMPLATE_CONTEXT_PROCESSORS = ('django.core.context_processors.auth',)
+
+ROOT_URLCONF = 'tests.urls'
+
+INSTALLED_APPS = (
+    'django.contrib.sites',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.comments',
+    'django.contrib.sessions',
+    'django.contrib.sitemaps',
+    'livesettings',
+    #'debug_toolbar',
+    'tests.localsite',
+    'django_extensions'
+)
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+DEBUG_TOOLBAR_CONFIG = {
+   'INTERCEPT_REDIRECTS' : False,
+}
+
+CACHE_PREFIX = 'T'
+CACHE_TIMEOUT = 300
