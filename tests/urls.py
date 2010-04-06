ff96262d52c181a1698fa282bd692ef11dc5f2e0
Bruce Kroeze <bkroeze@gmail.com>
2010-04-06 10:58:54 -0700

    fixing test urls

--- tests/urls.py
+++ tests/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns, include
 from django.contrib import admin
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^admin/settings/$', include('livesettings.urls'),
+    (r'^admin/settings/$', include('livesettings.urls')),
 )
