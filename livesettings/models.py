61d950fa3c45f1050cfc5bd43b30540c03903c60
Chris Moffitt <chris@moffitts.net>
2010-04-12 21:23:19 -0500

    Add args to SettingsNotSet Exception. Closes #6

--- livesettings/models.py
+++ livesettings/models.py
@@ -74,6 +74,7 @@ class SettingNotSet(Exception):
     def __init__(self, k, cachekey=None):
         self.key = k
         self.cachekey = cachekey
+        self.args = [self.key, self.cachekey]
 
 class SettingManager(models.Manager):
     def get_query_set(self):
