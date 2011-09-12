126a706e5e4c165dac15721581fb7c17057dbf15
Hynek Cernoch <hynek@sdb.cz>
2011-09-12 22:39:37 +0200

    Fixed tests for the web client - test_urls

---
+++ livesettings/test_urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls.defaults import *
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    (r'^settings/', include('livesettings.urls')),
+    (r'^admin/', include(admin.site.urls)),
+    (r'^accounts/login/', 'django.contrib.auth.views.login', {'template_name': 'admin/login.html'}),
+)
