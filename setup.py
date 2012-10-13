5a7643baf66aad39463cdd3b06ece97f57022ee5
Chris Moffitt <chris@moffitts.net>
2012-10-13 14:11:40 -0500

    Make sure to show messages. Bumping rev number.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = (1, 4, 11)
+VERSION = (1, 4, 12)
 
 # Dynamically calculate the version based on VERSION tuple
 if len(VERSION)>2 and VERSION[2] is not None:
