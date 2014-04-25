bda67999dab3113bc1b09525f76e33d2d9a1d32c
Chris Moffitt <chris@moffitts.net>
2014-04-24 19:07:02 -0500

    Bumping rev

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = (1, 4, 13)
+VERSION = (1, 4, 14)
 
 # Dynamically calculate the version based on VERSION tuple
 if len(VERSION)>2 and VERSION[2] is not None:
