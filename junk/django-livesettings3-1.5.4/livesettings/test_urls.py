4b8fa6af15742d9c0fd36f26301c69e547c540bd
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2023-07-30 13:21:53 -0400

    deletes irrelevant files

--- junk/django-livesettings3-1.5.4/livesettings/test_urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth import views as auth_views
-from django.conf.urls import url, include
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^settings/', include('livesettings.urls')),
-    url(r'^admin/', admin.site.urls),
-    url(r'^accounts/login/', auth_views.LoginView.as_view(template_name='admin/login.html'), name='loginview'),
-]
--- junk/django-livesettings3-1.5.4/livesettings/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from livesettings import views
-
-urlpatterns = [
-    url(r'^$', views.site_settings, name='satchmo_site_settings'),
-    url(r'^export/$', views.export_as_python, name='settings_export'),
-    url(r'^(?P<group>[^/]+)/$', views.group_settings, name='livesettings_group'),
-]
